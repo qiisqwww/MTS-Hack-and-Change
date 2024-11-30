805f37b5c0f65e5d0c7f6d4665f34bc948ee30ed
Balex <morevbalex777@gmail.com>
2024-11-30 13:37:42 +0300

    feat: add question popover

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { QuestionCircleOutlined } from "@ant-design/icons";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Layout from "./components/Layout/Layout";
 import LeftBoard from "./components/LeftBoard/LeftBoard";
+import { Popover } from "antd";
+
+const content = (
+  <div style={{ padding: 10 }}>
+    <p style={{ margin: 0 }}>
+      В строке ввода сообщения введите известную информацию
+      <br /> о сотруднике, которая могла бы быть в пункте "О себе". Так
+      <br /> же, вы можете использовать критерии для поиска, чтобы
+      <br /> сузить выдачу анкет.
+    </p>
+  </div>
+);
 
 function App() {
   return (
@@ -9,6 +22,11 @@ function App() {
       <main className="main">
         <LeftBoard />
         <Layout />
+        <div className="question">
+          <Popover content={content}>
+            <QuestionCircleOutlined className="question-icon" />
+          </Popover>
+        </div>
       </main>
       <Footer />
     </>
